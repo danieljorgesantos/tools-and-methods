148c7f137b435d156bb2e17e2206ff08bd03fa75
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-13 11:09:49 +0100

    added

---
+++ git/git.js
@@ -0,0 +1,56 @@
+// File: git.js
+
+// Git tool chain information
+// --------------
+// Git is a distributed version control system that tracks changes to files,
+// enabling collaboration and managing project history effectively.
+
+// Git Init
+// --------------
+// Description: Initializes a new Git repository in the current directory.
+// Usage: git init
+
+// Git Clone
+// --------------
+// Description: Creates a copy of a remote repository on your local machine.
+// Usage: git clone <repository>
+
+// Git Add
+// --------------
+// Description: Adds a file to the staging area, preparing it for a commit.
+// Usage: git add <file>
+
+// Git Commit
+// --------------
+// Description: Creates a new commit with the staged changes and attaches a descriptive message.
+// Usage: git commit -m "message"
+
+// Git Status
+// --------------
+// Description: Displays the current state of the repository, showing modified, staged, and untracked files.
+// Usage: git status
+
+// Git Pull
+// --------------
+// Description: Updates the local repository with the latest changes from the remote repository.
+// Usage: git pull
+
+// Git Push
+// --------------
+// Description: Uploads local commits to the remote repository.
+// Usage: git push
+
+// Git Branch
+// --------------
+// Description: Lists all branches in the repository.
+// Usage: git branch
+
+// Git Checkout
+// --------------
+// Description: Switches to a different branch.
+// Usage: git checkout <branch>
+
+// Git Merge
+// --------------
+// Description: Combines changes from a specified branch into the current branch.
+// Usage: git merge <branch>

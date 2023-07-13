95dee7b1685dddada97d52ac5b987f8231176f40
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-13 11:30:25 +0100

    added

--- git/git.js
+++ git/git.js
@@ -5,69 +5,73 @@
 // Git is a distributed version control system that tracks changes to files,
 // enabling collaboration and managing project history effectively.
 
-// Git Init
+// Git Commands
 // --------------
+// Description: Git commands to be used
+
+// Git Init
 // Description: Initializes a new Git repository in the current directory.
 // Usage: git init
 
 // Git Clone
-// --------------
 // Description: Creates a copy of a remote repository on your local machine.
 // Usage: git clone <repository>
 
 // Git Add
-// --------------
 // Description: Adds a file to the staging area, preparing it for a commit.
 // Usage: git add <file>
 
 // Git Add All
-// --------------
 // Description: Adds a file to the staging area, preparing it for a commit.
 // Usage: git add .
 
 // Git Commit
-// --------------
 // Description: Creates a new commit with the staged changes and attaches a descriptive message.
 // Usage: git commit -m "added"
 
 // Git Commit (-a)
-// --------------
 // Description: Creates a new commit with all modified and deleted files automatically staged, and attaches a descriptive message.
 // Usage: git commit -a -m "added"
 
+// Git Commands
+// Description: Git commands to be used
+
 // Git Status
-// --------------
 // Description: Displays the current state of the repository, showing modified, staged, and untracked files.
 // Usage: git status
 
 // Git Pull
-// --------------
 // Description: Updates the local repository with the latest changes from the remote repository.
 // Usage: git pull
 
 // Git Push
-// --------------
 // Description: Uploads local commits to the remote repository.
 // Usage: git push
 
 // Git Branch
-// --------------
 // Description: Lists all branches in the repository.
 // Usage: git branch
 
 // Git Checkout
-// --------------
 // Description: Switches to the newly created branch.
 // Usage: git checkout <branch-name>
 // Example: git checkout feature-branch
 
 // Git Checkout (-b)
-// --------------
 // Description: Creates a new branch and switches to it in one step.
 // Usage: git checkout -b <branch-name>
 // Example: git checkout -b feature-branch
 
 // Git Merge
-// --------------
 // Description: Combines changes from a specified branch into the current branch.
 // Usage: git merge <branch>
+
+// Consistent Naming Pattern for Branches
+// --------------
+// Description: To name branches with the number of the task convention in Git,
+// you can follow a consistent naming pattern.
+// Convention: feature/<task-number>-<task-description> or bugfix/<task-number>-<task-description>
+// Example 1: git branch feature/123-login
+// Example 2: git branch bugfix/456-search-bug
+
+

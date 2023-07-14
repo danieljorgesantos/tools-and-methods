c0b7e1b4838a8c6a8250847fbbdd6fd641315f91
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-14 01:27:01 +0100

    added

---
+++ javascript/e-angular/b-httpClient.js
@@ -0,0 +1,24 @@
+// Angular HTTP client Section
+// --------------
+// This Section contains information about the Angular HTTP Client.
+// The HTTP client in Angular is a built-in module that provides a high-level API for making HTTP requests from Angular applications.
+
+// Setting up
+
+// 1. Go to @NgModule of your current Module
+
+// 2. Add the import
+import { HttpClientModule } from '@angular/common/http';
+
+// 3. Add the import inside the @NgModule
+imports: [
+    BrowserModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule,
+]
+
+// creating a service in angular:
+ng generate service service-name
+
+
+

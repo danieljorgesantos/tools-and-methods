d97b512853974bfa9c71efda694b8023e2f86364
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-15 16:44:51 +0100

    added

--- documentation/d-scss.js
+++ documentation/d-scss.js
@@ -6,9 +6,22 @@
 // This module contains Example styles, Other Example Styles,
 // used in this application.
 
+// ------------------------------------------------------------------------------
+
 // Example Styles
 // --------------
 // These classes define styles for different example elements.
 
 // Class: example
 // Description: Represents a top-level example element.
+
+// ------------------------------------------------------------------------------
+
+// Example 2 Styles
+// --------------
+// These classes define styles for different example 2 elements.
+
+// Class: example_2
+// Description: Represents a top-level example 2 element.
+
+// ------------------------------------------------------------------------------

ff5b96f64f1b05364f23552e261ad83b6a5794c1
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-15 16:15:10 +0100

    added

---
+++ documentation/d-scss.js
@@ -0,0 +1,13 @@
+// File: _content.scss
+
+// Content Module
+// --------------
+// This module contains Typography styles, Images Styles,
+// Tables styles, and figure styles used in this application.
+
+// Typography Styles
+// --------------
+// These classes define styles for different typography elements.
+
+// Class: h1
+// Description: Represents a top-level heading.

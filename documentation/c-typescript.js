e6f7efc58ab6d69aab3542fd28ebac86632c2149
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-16 19:28:59 +0100

    added

--- documentation/c-typescript.js
+++ documentation/c-typescript.js
@@ -1,5 +1,7 @@
 // Angular Cli Section
 // --------------
 // Description: This section contains useful angular cli commands.
+// Type of Component: Dumb Component ( Presentational ).
+// Type of Component: Smart Component ( Container ).
 
 // Imports

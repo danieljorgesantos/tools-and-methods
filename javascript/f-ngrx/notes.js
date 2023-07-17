1c5c873396a3bdb07b2d0ee5041af323310d3444
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-07-17 16:24:01 +0100

    added

---
+++ javascript/f-ngrx/notes.js
@@ -0,0 +1,8 @@
+// NGRX
+// --------------
+// NgRx is a state management library for Angular applications that implements the Redux pattern.
+
+// Angular Instalation
+
+npm install @ngrx/store @ngrx/effects @ngrx/store-devtools
+

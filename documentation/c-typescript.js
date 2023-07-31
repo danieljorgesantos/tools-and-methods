44729cf131205b5e5f22711a648bf7d9f880c4a1
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-08-01 00:40:16 +0100

    added

--- documentation/c-typescript.js
+++ documentation/c-typescript.js
@@ -1,27 +1,27 @@
-// File: _example.scss
+// File: _example.ts
 
 // Example Module
 // ______________
 //
-// This module contains Example styles, Other Example Styles,
+// This module contains Example ts, Other Example ts,
 // used in this application.
 
 // Imports
 
 // ------------------------------------------------------------------------------
 
-// Example Styles
+// Example ts
 // --------------
-// These classes define styles for different example elements.
+// These classes define ts for different example elements.
 
 // Class: example
 // Description: Represents a top-level example element.
 
 // ------------------------------------------------------------------------------
 
-// Example 2 Styles
+// Example 2 ts
 // --------------
-// These classes define styles for different example 2 elements.
+// These classes define ts for different example 2 elements.
 
 // Class: example_2
 // Description: Represents a top-level example 2 element.
